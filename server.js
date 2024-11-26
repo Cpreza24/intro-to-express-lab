cd560d5196ff0172ce9a377c3d65c386c4b54951
Cesar Preza <cpreza93@yahoo.com>
2024-11-25 22:23:28 -0800

    completed step 3

--- server.js
+++ server.js
@@ -4,42 +4,62 @@ const morgan = require('morgan');
 const app = express();
 app.use(morgan('dev'));
 
+app.get('/', (req, res) => {
+    res.send('Root Route');
+});
+
 
 app.get('/greetings/:userName', (req, res) => {
     res.send(`Hello there ${req.params.userName}`);
-})
+});
 
-// check if the parameter passed is a number
-// if the parameter is not a number return message "you must specify a number"
-// if the paramater is a number, the response should be a random number between 0 and the number in the paramater. 
 
 app.get('/roll/:number', (req, res) => {
-    paramToNumber = parseInt(req.params.number);
+    const paramToNumber = parseInt(req.params.number);
     const randomNum = Math.floor(Math.random() * req.params.number + 1);
-    const stringParam = req.params.number;
 
-    if (typeof(stringParam) === 'string') {
-        res.send('Please enter a number');
-        console.log(typeof(stringParam));
-    } else if (typeof(req.params.number) === 'number') {
-        res.send(`you rolled a ${randomNum}`);
-    }
+    if (!isNaN(paramToNumber)) {
+        res.send(`You rolled a ${randomNum}`);
+    } else {
+        res.send('You must specify a number');
+    };
+});
+
+const collectibles = [
+    { name: 'shiny ball', price: 5.95 },
+    { name: 'autographed picture of a dog', price: 10 },
+    { name: 'vintage 1970s yogurt SOLD AS-IS', price: 0.99 }
+];
 
 
+app.get('/collectibles/:id', (req, res) => {
+    const collectibleId = parseInt(req.params.id);
+    
+    if (req.params.id > collectibles.length) {
+        res.send(`This item is not yet in stock`)
+    } else (
+        res.send(`So, you want the ${collectibles[collectibleId].name} For ${collectibles[collectibleId].price}, it can be yours!`)
+    )
+});
+
+const shoes = [
+    { name: "Birkenstocks", price: 50, type: "sandal" },
+    { name: "Air Jordans", price: 500, type: "sneaker" },
+    { name: "Air Mahomeses", price: 501, type: "sneaker" },
+    { name: "Utility Boots", price: 20, type: "boot" },
+    { name: "Velcro Sandals", price: 15, type: "sandal" },
+    { name: "Jet Boots", price: 1000, type: "boot" },
+    { name: "Fifty-Inch Heels", price: 175, type: "heel" }
+];
+
+app.get('/shoes', (req, res) => {
+    const queryParam = req.query.shoes;
+    res.send(`${queryParam[shoes].name}`)
 })
 
-// app.get('/roll/:string', (req, res) => {
-//     res.send('you must specify a number');
-// })
 
 
 
-
-
-
-
-
-
-app.listen(3000, (req, res) => {
-    console.log('listening on port 3000')
+app.listen(3001, (req, res) => {
+    console.log('listening on port 3001')
 });
